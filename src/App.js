a168a735ff0b0e80bad76e509f39e6fe197ba47f
Theresa Thoraldson <theresa.thoraldson@outlook.com>
2022-04-01 17:07:25 -0400

    added icons

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { faTwitter, faGithub, faKaggle, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 import './App.css';
+
+library.add(faEnvelope, faTwitter, faGithub, faKaggle, faLinkedin)
 
 function App() {
   return (
@@ -7,11 +12,16 @@ function App() {
       <header className="App-header">
         <h1>Hello! It was nice to meet you.</h1>
         <h2>Theresa Thoraldson - Software Engineer</h2>
-        <p>Cool stuff about me!</p>
-        <p>Have a look at my <a href='www.mycoolresume.com'>resume</a> if you'd like to know more.</p>
+        <p>I'm currently a Software Engineering Manager at <a href='https://www.serko.com/'>Serko</a>, working on modernizing the corporate expensing experience. I have 5 years of professional software engineering experience.</p>
+        <p>I'm also going through <a href='https://www.eacambridge.org/technical-alignment-curriculum'>EA Cambridge AI Safety Fellowship</a>.</p>
+        {/* <p>Have a look at my <a href='www.mycoolresume.com'>resume</a> if you'd like to know more.</p> */}
         <h2>Let's keep in touch.</h2>
         <div id="icons">
-        <FontAwesomeIcon icon="fa-brands fa-github" />
+        <a href="mailto:theresa.thoraldson@gmail.com"><FontAwesomeIcon icon={["fa", "envelope"]} size="20px"/></a>
+          <a href="https://twitter.com/tthoraldson"><FontAwesomeIcon icon={["fab", "twitter"]} /></a>
+          <a href="https://www.linkedin.com/in/theresathoraldson/"><FontAwesomeIcon icon={["fab", "linkedin"]} /></a>
+          <a href="https://github.com/tthoraldson"><FontAwesomeIcon icon={["fab", "github"]} /></a>
+          <a href="https://www.kaggle.com/tthoraldson/"><FontAwesomeIcon icon={["fab", "kaggle"]} /></a>
         </div>
       </header>
     </div>
